786d95c40c72f829fae24709783c4bd4ee9ee4d2
bertrandLl <bertrand.llorenss@gmail.com>
2017-02-20 18:53:01 +0100

    ihm bertrand

--- src/vues/TheMain.java
+++ src/vues/TheMain.java
@@ -5,15 +5,19 @@ public class TheMain {
 
 	public static void main(String[] args) {
 		// Test de la vue personne
-		JFrame fenetre = new JFrame();
-		PersonneJPA c = new PersonneJPA();
-		fenetre.setContentPane(c);
-		fenetre.setVisible(true);
-		
-		// Test de la vue compétences
-		JFrame fenetreCpt = new JFrame();
-		CompetenceJP cpt = new CompetenceJP();
-		fenetreCpt.setContentPane(cpt);
-		fenetreCpt.setVisible(true);
+		Principale p = new Principale();
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
