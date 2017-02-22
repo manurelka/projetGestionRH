29ce0a97b6c6edc1d99df84674621ad6f9615da9
bertrandLl <bertrand.llorenss@gmail.com>
2017-02-22 10:11:01 +0100

    The main

--- src/vues/TheMain.java
+++ src/vues/TheMain.java
@@ -1,11 +1,9 @@
 package vues;
-import javax.swing.JFrame;
 
 public class TheMain {
 
 	public static void main(String[] args) {
-		// Test de la vue personne
-		Principale p = new Principale();
+		
 	}
 }
 
