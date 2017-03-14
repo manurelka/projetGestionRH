d4ed43750299c76a6813d16e3df3b46d61b26eff
manurelka <diazmanuela89@gmail.com>
2017-03-14 08:21:36 +0100

    Accessor personnel

---
+++ src/reader_writer/IHMPersonnelAccessor.java
@@ -0,0 +1,13 @@
+package reader_writer;
+
+import ressources.IModifEcouteur;
+import ressources.ListePersonnes;
+
+public interface IHMPersonnelAccessor {
+	public static final LecteurPersonel LP = LecteurPersonel.Instance();
+	//TODO public static final EcriteurPersonnel EP = EcriteurPersonnel.Instance();
+	public static ListePersonnes personnes_init = LP.lirePersonnel();
+	public static void addModifEcouteur(IModifEcouteur e){
+		//TODO personnes_init.addModifEcouteur(e);
+	}
+}
