0ae82f41d078d260c3656ba40b87280379845cc9
manurelka <diazmanuela89@gmail.com>
2017-03-20 08:33:40 +0100

    Correction lecture compétences : on retrouve les compétences

--- src/reader_writer/SplitterPersCmp.java
+++ src/reader_writer/SplitterPersCmp.java
@@ -6,7 +6,7 @@ import ressources.ListeCompetences;
 public class SplitterPersCmp extends ObjectSplitter implements ISplitterPersCmp{
 	private static final int NB_ID = 0;
 	private static final int NB_PREM_CMP = 1;
-	private SplitterCodeDomaine pointSplitter;
+	private SplitterCodeDomaine pointSplitter = new SplitterCodeDomaine();
 
 	protected SplitterPersCmp() {
 		super();
@@ -25,11 +25,11 @@ public class SplitterPersCmp extends ObjectSplitter implements ISplitterPersCmp{
 	@Override
 	public ListeCompetences getCompetences() {
 		ListeCompetences rendu = new ListeCompetences();
-		for( int i =NB_PREM_CMP; i<getLength(); i++){
+		for( int i =NB_PREM_CMP; i < getLength(); i++){
 			DCCompetence cmp;
 			pointSplitter.splitLine(getMot(i));
 			cmp = new DCCompetence(pointSplitter.getDomaineDCComp(), pointSplitter.getCodeInt());
-			rendu.ajouter(IHMCompetencesAccessor.competences_init.get(getMot(i)));
+			rendu.ajouter(IHMCompetencesAccessor.competences_init.get(cmp));
 		}
 		return rendu;
 	}
